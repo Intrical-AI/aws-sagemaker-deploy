69b5928a23f63864b02366eb76cd57111339cffe
gabriele-bani <bani.gabri@gmail.com>
2022-01-27 14:00:26 +0100

    use latest as image tag to push

--- sagemaker/generate_docker_image_tag.py
+++ sagemaker/generate_docker_image_tag.py
@@ -1,3 +1,4 @@
 def generate_docker_image_tag(registry_uri, bento_name, bento_version):
-    image_tag = f"{bento_name}-{bento_version}".lower()
+    # image_tag = f"{bento_name}-{bento_version}".lower()
+    image_tag = "latest"
     return f"{registry_uri}:{image_tag}"
